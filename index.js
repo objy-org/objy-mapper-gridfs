e888c68de582aed360c5e77e3a0fcd1d06d48626
Benjamin Lotterer <theedgesofben@hotmail.com>
2025-08-13 11:22:36 +0200

    Replace mongoose-gridfs package with native gridfs

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var mongoose = require('mongoose');
-var Grid = require('gridfs-stream');
 var Schema = mongoose.Schema;
 var Admin = mongoose.mongo.Admin;
-var shortid = require('shortid');
 
-var createModel;
-
-var Attachment;
-
-var generalObjectModel = {
+const generalObjectModel = {
     type: { type: String, index: true },
     applications: { type: [String], index: true },
     created: { type: String, index: true },
@@ -29,180 +23,172 @@ var generalObjectModel = {
     username: String,
     email: String,
     _clients: [],
-    authorisations: {}
+    authorisations: {},
 };
 
-var ObjSchema = new Schema(generalObjectModel, { strict: false });
+const ObjSchema = new Schema(generalObjectModel, { strict: false });
 
-const mongo = mongoose.mongo;
-
-Mapper = function(OBJY, options) {
+Mapper = function (OBJY, options) {
     return Object.assign(new OBJY.StorageTemplate(OBJY, options), {
-
         database: {},
         index: {},
         globalPaging: 20,
 
-        connect: function(connectionString, success, error, options) {
+        connect: function (connectionString, success, error, options) {
             this.database = mongoose.createConnection(connectionString, options);
 
-            this.database.on('error', function(err) {
-                error(err)
+            this.database.on('error', function (err) {
+                error(err);
             });
 
-            this.database.once('open', function() {
+            this.database.once('open', function () {
                 success();
-                createModel = require('mongoose-gridfs').createModel;
             });
 
             return this;
         },
 
-        getConnection: function() {
+        getConnection: function () {
             return this.database;
         },
 
-        useConnection: function(connection, success, error) {
+        useConnection: function (connection, success, error) {
             this.database = connection;
 
-            this.database.on('error', function(err) {
-                error(err)
+            this.database.on('error', function (err) {
+                error(err);
             });
 
-            this.database.once('open', function() {
+            this.database.once('open', function () {
                 success();
             });
 
             return this;
         },
 
-        getDBByMultitenancy: function(client) {
-
+        getDBByMultitenancy: function (client) {
             if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED) {
-                return this.database.useDb('spoo')
+                return this.database.useDb('spoo');
             } else if (this.multitenancy == this.CONSTANTS.MULTITENANCY.ISOLATED) {
-                return this.database.useDb(client)
+                return this.database.useDb(client);
             }
         },
 
-        createClient: function(client, success, error) {
-
+        createClient: function (client, success, error) {
             var db = this.getDBByMultitenancy(client);
 
             var ClientInfo = db.model('clientinfos', ClientSchema);
 
-            ClientInfo.find({ name: client }).exec(function(err, data) {
+            ClientInfo.find({ name: client }).exec(function (err, data) {
                 if (err) {
                     error(err);
                     return;
                 }
                 if (data.length >= 1) {
-                    error("client name already taken")
+                    error('client name already taken');
                 } else {
-
-                    new ClientInfo({ name: client }).save(function(err, data) {
+                    new ClientInfo({ name: client }).save(function (err, data) {
                         if (err) {
-
                             error(err);
                             return;
                         }
 
                         success(data);
-
-                    })
+                    });
                 }
-
             });
         },
 
-        listClients: function(success, error) {
-
+        listClients: function (success, error) {
             if (this.multitenancy == this.CONSTANTS.MULTITENANCY.ISOLATED) {
-
-                new Admin(this.database.db).listDatabases(function(err, result) {
-                    if (err) error(err)
-                    success(result.databases.map(function(item) {
-                        return item.name
-                    }));
+                new Admin(this.database.db).listDatabases(function (err, result) {
+                    if (err) error(err);
+                    success(
+                        result.databases.map(function (item) {
+                            return item.name;
+                        })
+                    );
                 });
-
             } else {
                 var db = this.getDBByMultitenancy('spoo');
 
                 var ClientInfo = db.model('clientinfos', ClientSchema);
 
-                ClientInfo.find({}).exec(function(err, data) {
-
+                ClientInfo.find({}).exec(function (err, data) {
                     if (err) {
-
                         error(err);
                         return;
                     }
 
-                    success(data.map(function(item) {
-                        return item.name
-                    }))
-
+                    success(
+                        data.map(function (item) {
+                            return item.name;
+                        })
+                    );
                 });
             }
         },
 
-        getById: function(id, success, error, app, client) {
+        getById: async function (id, success, error, app, client) {
+            const db = this.getDBByMultitenancy(client);
+            const bucket = new mongoose.mongo.GridFSBucket(db);
 
-            var db = this.getDBByMultitenancy(client);
+            let data = null;
 
-            var Attachment = createModel({
-                modelName: 'File',
-                connection: db
-            });
             var constrains = null;
 
             try {
-                constrains = { _id: mongoose.Types.ObjectId(id) }; 
-            } catch(e){
+                constrains = { _id: id };
+            } catch (e) {
                 OBJY.Logger.error(e);
-                error('Error getting file invalid id')
-                return
+                error('Error getting file invalid id');
+                return;
             }
 
-            if (app) constrains['applications'] = { $in: [app] }
+            if (app) constrains['applications'] = { $in: [app] };
 
             if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED && client) constrains['tenantId'] = client;
 
             Obj = db.model(this.objectFamily, ObjSchema);
 
             try {
-                var _data = Attachment.read({ _id: mongoose.Types.ObjectId(id) })
+                //var _data = Attachment.read({ _id: mongoose.Types.ObjectId(id) });
+                let downloadStream = null;
 
-                Obj.findOne(constrains, function(err, data) {
-                    if (err) {
-                        error(err);
-                        return;
-                    }
-
-                    if(!data) return error({error: 'file not found'});
-
-                    if (!data.properties) data.properties = {};
-
-                    data.properties.data = _data;
-
-                    success(data);
+                try {
+                    downloadStream = bucket.openDownloadStream(new mongoose.Types.ObjectId(id));
+                } catch (err) {
+                    error(err);
                     return;
-                });
+                }
+
+                try {
+                    data = await Obj.findOne(constrains);
+                } catch (err) {
+                    error(err);
+                    return;
+                }
+
+                if (!data) return error({ error: 'file not found' });
+
+                if (!data.properties) data.properties = {};
+
+                data.properties.data = downloadStream;
+
+                success(data);
+                return;
 
                 //success({ _id: id, name: id, role: this.objectFamily, properties: { data: data } });
             } catch (e) {
                 OBJY.Logger.error(e);
-                error('Error getting file')
+                error('Error getting file');
             }
         },
 
-        getByCriteria: function(criteria, success, error, app, client, flags) {
-
-            var db = this.getDBByMultitenancy(client);
+        getByCriteria: function (criteria, success, error, app, client, flags) {
+            const db = this.getDBByMultitenancy(client);
 
             var Obj = db.model(this.objectFamily, ObjSchema);
-
 
             if (flags.$page == 1) flags.$page = 0;
             else flags.$page -= 1;
@@ -215,33 +201,36 @@ Mapper = function(OBJY, options) {
             }
 
             var arr = [{ $match: criteria }, { $limit: 20 }];
-            if (flags.$page) arr.push({ $skip: this.globalPaging * (flags.$page || 0) })
+            if (flags.$page) arr.push({ $skip: this.globalPaging * (flags.$page || 0) });
 
             var s = {};
 
             if (flags.$sort) {
-
                 if (flags.$sort.charAt(0) == '-') {
                     s[flags.$sort.slice(1)] = -1;
                 } else {
                     s[flags.$sort] = 1;
                 }
 
-                arr.push({ $sort: s })
+                arr.push({ $sort: s });
             }
 
-            if (app) criteria['applications'] = { $in: [app] }
+            if (app) criteria['applications'] = { $in: [app] };
 
-            var finalQuery = Obj.find(criteria)
+            var finalQuery = Obj.find(criteria);
 
-            if (flags.$limit) finalQuery.limit(flags.$limit).sort(s || { '_id': 1 });
-            else finalQuery.limit(this.globalPaging).skip(this.globalPaging * (flags.$page || 0)).sort(s || { '_id': 1 });
+            if (flags.$limit) finalQuery.limit(flags.$limit).sort(s || { _id: 1 });
+            else
+                finalQuery
+                    .limit(this.globalPaging)
+                    .skip(this.globalPaging * (flags.$page || 0))
+                    .sort(s || { _id: 1 });
 
             if (criteria.$aggregate) {
                 finalQuery = Obj.aggregate(criteria.$aggregate);
             }
 
-            finalQuery.lean().exec(function(err, data) {
+            finalQuery.lean().exec(function (err, data) {
                 if (err) {
                     error(err);
                     return;
@@ -250,13 +239,9 @@ Mapper = function(OBJY, options) {
                 success(data);
                 return;
             });
-
-
-
         },
 
-        count: function(criteria, success, error, app, client, flags) {
-
+        count: function (criteria, success, error, app, client, flags) {
             var db = this.getDBByMultitenancy(client);
 
             var Obj = db.model(this.objectFamily, ObjSchema);
@@ -266,58 +251,98 @@ Mapper = function(OBJY, options) {
                 delete criteria.$query;
             }
 
-            if (app) criteria['applications'] = { $in: [app] }
+            if (app) criteria['applications'] = { $in: [app] };
 
             if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED && client) criteria['tenantId'] = client;
 
-            Obj.count(criteria).exec(function(err, data) {
+            Obj.count(criteria).exec(function (err, data) {
                 if (err) {
                     error(err);
                     return;
                 }
 
-                success({ 'result': data });
+                success({ result: data });
                 return;
             });
         },
 
-        update: function(spooElement, success, error, app, client) {
-
-            error('Update not possible')
+        update: function (spooElement, success, error, app, client) {
+            error('Update not possible');
         },
 
-        add: function(spooElement, success, error, app, client) {
+        add: async function (spooElement, success, error, app, client) {
+            const db = this.getDBByMultitenancy(client);
+            const bucket = new mongoose.mongo.GridFSBucket(db);
 
-            var db = this.getDBByMultitenancy(client);
-
-            var Attachment = createModel({
-                modelName: 'File',
-                connection: db
-            });
+            let data = null;
 
             //spooElement = spooElement.properties.data;
 
             var Obj = db.model(this.objectFamily, ObjSchema);
 
             try {
-
                 var fileId = new mongoose.Types.ObjectId();
 
                 var fileData = spooElement.properties.data;
 
-                Attachment.write({ _id: fileId, filename: spooElement.name }, fileData, (err, file) => {
-                    
+                const uploadStream = bucket.openUploadStream(spooElement.name, {
+                    metadata: {},
+                });
 
+                try {
+                    await new Promise((resolve, reject) => {
+                        console.log(db);
+
+                        fileData
+                            .pipe(uploadStream)
+                            .on('error', (err) => {
+                                console.log(err);
+                                reject(err);
+                            })
+                            .on('finish', (data) => {
+                                console.log('finish', uploadStream.id);
+                                resolve(data);
+                            });
+                    });
+                } catch (err) {
+                    console.log(err);
+                    error('Error adding file');
+                    return;
+                }
+
+                var _spooElement = Object.assign({}, spooElement);
+
+                if (uploadStream.id) _spooElement._id = uploadStream.id.toString();
+                else _spooElement._id = fileId.toString();
+
+                if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED) _spooElement.tenantId = client;
+
+                delete _spooElement.properties.data;
+                _spooElement.mimetype = _spooElement.properties.mimetype + '';
+                delete _spooElement.properties.mimetype;
+
+                try {
+                    data = await Obj(_spooElement).save();
+                } catch (err) {
+                    error(err);
+                    return;
+                }
+
+                console.log(data);
+
+                success(data);
+
+                /*
+                Attachment.write({ _id: fileId, filename: spooElement.name }, fileData, (err, file) => {
                     if (err) {
-                        console.log(err); 
+                        console.log(err);
                         error('Error adding file');
                         return;
                     }
 
+                    var _spooElement = Object.assign({}, spooElement);
 
-                    var _spooElement = Object.assign({}, spooElement)
-
-                    if(file) _spooElement._id = file._id.toString();
+                    if (file) _spooElement._id = file._id.toString();
                     else _spooElement._id = fileId.toString();
 
                     if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED) _spooElement.tenantId = client;
@@ -325,68 +350,57 @@ Mapper = function(OBJY, options) {
                     delete _spooElement.properties.data;
                     _spooElement.mimetype = _spooElement.properties.mimetype + '';
                     delete _spooElement.properties.mimetype;
-                    new Obj(_spooElement).save(function(err, data) {
+                    new Obj(_spooElement).save(function (err, data) {
                         if (err) {
                             error(err);
                             return;
                         }
                         success(data);
-                    })
+                    });
+               
 
                     //success({ _id: file._id, name: fileId })
                 });
-
+                */
             } catch (e) {
                 OBJY.Logger.error(e);
-                error('Error adding file')
+                error('Error adding file');
             }
         },
-        remove: function(spooElement, success, error, app, client) {
+        remove: async function (spooElement, success, error, app, client) {
+            const db = this.getDBByMultitenancy(client);
+            const bucket = new mongoose.mongo.GridFSBucket(db);
 
-            var db = this.getDBByMultitenancy(client);
-
-            var Attachment = createModel({
-                modelName: 'File',
-                connection: db
-            });
-
+            let data = null;
 
             var Obj = db.model(this.objectFamily, ObjSchema);
 
             var criteria = { _id: spooElement._id };
 
-            if (app) criteria['applications'] = { $in: [app] }
+            if (app) criteria['applications'] = { $in: [app] };
 
             if (this.multitenancy == this.CONSTANTS.MULTITENANCY.SHARED && client) criteria['tenantId'] = client;
 
+            try {
+                data = await bucket.delete(new mongoose.Types.ObjectId(spooElement._id));
+            } catch (err) {
+                OBJY.Logger.error(err);
+                error('Error removing file');
+                return;
+            }
 
-
-
-            Attachment.unlink({ _id: spooElement._id }, (err, file) => {
+            Obj.deleteOne(criteria, function (err, data) {
                 if (err) {
-                    OBJY.Logger.error(err);
-                    error('Error removing file');
-                    return
+                    error(err);
+                    return;
                 }
-
-                Obj.deleteOne(criteria, function(err, data) {
-                    if (err) {
-                        error(err);
-                        return;
-                    }
-                    if (data.n == 0) error("object not found");
-                    else {
-                        success({ _id: spooElement._id });
-                    }
-
-                })
-
-                //success({ _id: spooElement._id })
+                if (data.n == 0) error('object not found');
+                else {
+                    success({ _id: spooElement._id });
+                }
             });
-        }
-    })
-}
-
-
+        },
+    });
+};
 
 module.exports = Mapper;
